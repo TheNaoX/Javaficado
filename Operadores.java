4514f66d7ed66ac25469ba81907cc241e6a9c8fd
Antonio Chavez <cavjzz@gmail.com>
2014-11-24 20:54:16 -0600

    Agregado primer programa de operadores aritmeticos

---
+++ Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String []args) {
+      int i = 10;
+      System.out.println(i+10); // 20
+      System.out.println(i-10); // 0
+      System.out.println(i*10); // 100
+      System.out.println(i/10); // 1
+      System.out.println(i%10); // 0
+      System.out.println(i++);  // 11
+      System.out.println(i--);  // 9
+    }
+}
