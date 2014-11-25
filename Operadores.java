186d685b2d1d34b660eabcd64e4769fae3af8af3
Antonio Chavez <cavjzz@gmail.com>
2014-11-24 21:24:05 -0600

    Modificado el archivo con los operadores de asignación

--- Operadores.java
+++ Operadores.java
@@ -1,12 +1,24 @@
 public class Operadores {
-    public static void main(String []args) {
-      int a = 10, b = 20;
+  public static void main(String []args) {
+    float a = 10, b = 20, c = 0;
 
-      System.out.println(a == b); // false
-      System.out.println(a != b); // true
-      System.out.println(a > b);  // false
-      System.out.println(a < b);  // true
-      System.out.println(a >= b); // false
-      System.out.println(a <= b); // true
-    }
+    c = a + b;
+    System.out.println(c); // 30
+
+    c += a;
+    System.out.println(c); // 40
+
+    c -= a;
+    System.out.println(c); // 30
+
+    c *= a;
+    System.out.println(c); // 300
+
+    c /= a;
+    System.out.println(c); // 30
+
+    c %= a;
+    System.out.println(c); // 0
+  }
 }
+
